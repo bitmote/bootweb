eb06d0590a9434eb4335a129de6177dfd46d0ee4
lg-yt <1057935399@qq.com>
2018-05-17 13:56:17 +0800

    rm netty

--- src/main/java/com/luangeng/bootweb/StartApplication.java
+++ src/main/java/com/luangeng/bootweb/StartApplication.java
@@ -1,32 +1,18 @@
 package com.luangeng.bootweb;
 
 import com.alibaba.druid.pool.DruidDataSource;
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.luangeng.bootweb.config.HttpPortCfg;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.PlatformTransactionManager;
-import com.luangeng.bootweb.config.HttpPortCfg;
-import com.luangeng.bootweb.config.NettyServerConfig;
-import com.luangeng.bootweb.server.HttpNettyServer;
 
 import javax.annotation.Resource;
 import javax.sql.DataSource;
@@ -40,12 +26,8 @@ public class StartApplication {
     @Resource
     private HttpPortCfg httpPortCfg;
 
-    @Resource
-    private NettyServerConfig nettyConfig;
-
     public static void main(String[] args) throws Exception{
         SpringApplication app = new SpringApplication(StartApplication.class);
-        app.setBannerMode(Banner.Mode.OFF);
         app.run(args);
     }
 
@@ -73,30 +55,5 @@ public class StartApplication {
     public PlatformTransactionManager transactionManager() {
         return new DataSourceTransactionManager(dataSource());
     }
-
-//    // 配置8080端口自动转向8443
-//    @Bean
-//    public EmbeddedServletContainerFactory servletContainer() {
-//        TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory() {
-//            @Override
-//            protected void postProcessContext(Context context) {
-//                SecurityConstraint securityConstraint = new SecurityConstraint();
-//                securityConstraint.setUserConstraint("CONFIDENTIAL");
-//                SecurityCollection collection = new SecurityCollection();
-//                collection.addPattern("/*");
-//                securityConstraint.addCollection(collection);
-//                context.addConstraint(securityConstraint);
-//            }
-//        };
-//
-//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-//        connector.setScheme("http");
-//        connector.setPort(Integer.valueOf(httpPortCfg.getHttp_port()));
-//        connector.setSecure(false);
-//        connector.setRedirectPort(Integer.valueOf(httpPortCfg.getHttps_port()));
-//
-//        tomcat.addAdditionalTomcatConnectors(connector);
-//        return tomcat;
-//    }
 
 }
